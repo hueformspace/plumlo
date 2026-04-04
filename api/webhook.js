694f214da36777073fca3c62615bf27accada5de
hueformspace <hueformspace@gmail.com>
2026-04-04 08:50:28 -0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
-import { Resend } from 'npm:resend';
+import { Resend } from 'resend';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
